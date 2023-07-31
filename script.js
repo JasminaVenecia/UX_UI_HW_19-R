1992f05a3e2a34a0166a159724063128e19a1e0f
JasminaVenecia <126337401+JasminaVenecia@users.noreply.github.com>
2023-07-30 21:11:47 -0400

    1st Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+        $(document).ready(function() {
+            $('.zoom-image').hover(function() {
+                $(this).css('transform', 'scale(1.2)'); 
+            }, function() {
+                $(this).css('transform', 'scale(1)');
+ </script>
+       
